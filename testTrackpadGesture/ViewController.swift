b623fe5d833b27c2a61938330323eaafc95917c2
James Tang <james@goodnotesapp.com>
2020-05-08 13:55:29 +0800

    Initial Commit

---
+++ testTrackpadGesture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testTrackpadGesture
+//
+//  Created by James Tang on 8/5/2020.
+//  Copyright © 2020 James Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ testTrackpadGestureTests/testTrackpadGestureTests.swift
@@ -0,0 +1,34 @@
+//
+//  testTrackpadGestureTests.swift
+//  testTrackpadGestureTests
+//
+//  Created by James Tang on 8/5/2020.
+//  Copyright © 2020 James Tang. All rights reserved.
+//
+
+import XCTest
+@testable import testTrackpadGesture
+
+class testTrackpadGestureTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
